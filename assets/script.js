b3939a049afe405e42e1810a7f2efcfd449e1471
mac <mac@macs-iMac.local>
2021-01-03 15:15:31 -0500

    character types

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var generatePassword = function () {
 
-    // Prompt #1: ask for password length
+    // Prompt #1: password length
     var invalidLength = true; // The output of this window prompt will be a string, thus an invalid length
   
     while (invalidLength) {
@@ -22,3 +22,54 @@ var generatePassword = function () {
       
       }
     }
+
+      // Prompt #2: array
+  var charTypes = [
+    {
+      name: "lowercase",
+      included: true,
+      chars: "abcdefghijklmnopqrstuvwxyz"
+    },
+    {
+      name: "UPPERCASE",
+      included: true,
+      chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    },
+    {
+      name: "numeric",
+      included: true,
+      chars: "0123456789"
+    },
+    {
+      name: "special",
+      included: true,
+      chars: " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+    },
+  ];
+   
+   var noneSelected = true; // Since none are selected this will be true, so we can enter the while loop
+
+   while (noneSelected) {
+     // have a running "list" of characters that are acceptable (it is really a string that will be added onto)
+     // this string will start as empty
+     var acceptableChars = "";
+     // loop through the array of objects
+     for (i = 0; i < charTypes.length; i++) {
+       var includedConfirm = window.confirm("Would you like your password to include " + charTypes[i].name + " characters?");
+       if (includedConfirm) {
+         charTypes[i].included = true; 
+         
+         acceptableChars = acceptableChars + charTypes[i].chars
+         
+         noneSelected = false
+       } else {
+         charTypes[i].included = false;
+       };
+     };
+ 
+     // Give an alert if none of the options were selected
+     if (noneSelected) {
+       window.alert("Please make sure you select atleast one of the character types.")
+     };
+   };
+   
